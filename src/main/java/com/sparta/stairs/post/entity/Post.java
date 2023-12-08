281e5fff2f81cd98d0e3006a42f28aaca19020a9
yeongdo99 <atpsfka1@gmail.com>
2023-12-08 17:59:01 +0900

    feat : Post Entity 추가

---
+++ src/main/java/com/sparta/stairs/post/entity/Post.java
@@ -0,0 +1,51 @@
+package com.sparta.stairs.post.entity;
+
+import com.sparta.stairs.global.Time;
+import com.sparta.stairs.post.dto.PostRequestDto;
+import com.sparta.stairs.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post extends Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> commentList = new ArrayList<>();
+
+    public Post(PostRequestDto requestDto, User user) {
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+        this.user = user;
+
+    }
+
+
+    public void update(PostRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+        this.modifiedDateTime = ZonedDateTime.now();
+    }
+}
