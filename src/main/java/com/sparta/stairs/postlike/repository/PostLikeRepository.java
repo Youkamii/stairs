9c084391214286a7e55a553472587a08c5864050
MOON <moonkihyun95@gmail.com>
2023-12-08 15:59:36 +0900

    feature : 게시글 좋아요,  댓글 좋아요

---
+++ src/main/java/com/sparta/stairs/postlike/repository/PostLikeRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.stairs.postlike.repository;
+
+import com.sparta.stairs.postlike.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+
+    Optional<PostLike> findByUserAndPost(User user, Post post);
+}
---
+++ src/main/java/com/sparta/stairs/postlike/service/PostLikeService.java
@@ -0,0 +1,38 @@
+package com.sparta.stairs.postlike.service;
+
+import com.sparta.stairs.postlike.entity.PostLike;
+import com.sparta.stairs.postlike.repository.PostLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+
+    private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+
+
+    public ResponseEntity<CommonResponseDto> addPostLike(Long postId, User user) {
+        Post post = postRepository.findById(postId).orElseThrow(() ->
+                new NullPointerException("해당 게시물은 존재하지 않습니다."));
+
+        if (user.getUserId().equals(post.getUser().getUserId())) {
+            throw new IllegalArgumentException("자신이 작성한 게시물에는 좋아요를 누를 수 없습니다.");
+        }
+
+        PostLike postLike = postLikeRepository.findByUserAndPost(user, post).orElse(null);
+
+        if (postLike == null) {
+            postLikeRepository.save(new PostLike(user, post));
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponseDto(postId + "번 게시물 좋아요 등록", HttpStatus.CREATED.value()));
+        } else {
+            postLikeRepository.delete(postLike);
+
+            return ResponseEntity.status(HttpStatus.OK).body(new CommonResponseDto(postId + "번 게시물 좋아요 취소", HttpStatus.OK.value()));
+        }
+    }
+}
