b77a362aef3b26e20795797481c5d87d4597a5ed
yeongdo99 <atpsfka1@gmail.com>
2023-12-08 18:00:05 +0900

    feat : Post Reposity 추가

---
+++ src/main/java/com/sparta/stairs/post/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.stairs.post.repository;
+
+import com.sparta.stairs.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
